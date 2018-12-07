f0799d683515dfe4488e90782461ea363081b7a1
PKPurdue <pkrivack@purdue.edu>
2018-12-07 01:41:40 -0500

    players can create games now
    game functionality created on frontend and backend

--- public/js/main.js
+++ public/js/main.js
@@ -21,7 +21,7 @@ socket.on('usernameResponse', function(data)
 	}
 });
 
-function showServers()
+function verifyName()
 {
 	var nam = $(".nameBox")[0].value;
 	if (nam == undefined || nam.length < 4 || nam.length > 20)
@@ -35,3 +35,73 @@ function showServers()
 	}
 }
 
+socket.on('createServerResponse', function(data)
+{
+	$("#serverList")[0].style.display = "none";
+	$("#serverLobby")[0].style.display = "block";
+});
+
+function createGame()
+{
+	var serverName = $("#createServerName")[0].value;
+	if (serverName == undefined || serverName.length < 1 || serverName.length > 20)
+	{
+		alert("The server name must be between 1 and 20 characters");
+		return;
+	}
+	socket.emit('createServer', {serverN: serverName});
+}
+
+socket.on('startGameResponse', function(data)
+{
+	if (data.success == true)
+	{
+		alert("Game is starting");
+	}
+	else
+	{
+		alert("Only the host can start the game");
+	}
+});
+
+function joinServer(gameButton)
+{
+	console.log(gameButton);
+	var game = gameButton.parentNode;
+	socket.emit('joinGame', {serverName: game.children[0].innerHTML});
+}
+
+function showJoinableGames()
+{
+	socket.emit('getJoinableGames', {});
+}
+
+socket.on('getJoinableGamesResponse', function(data)
+{
+	var currentServers = $("#gameServer");
+	for (var i = 0; i < currentServers.length; i++) { currentServers[i].remove(); }
+	
+	var games = data.games;
+	//console.log(JSON.stringify(games));
+	for (var i = 0; i < games.length; i++)
+	{
+		var clon = $("#serverExample")[0].cloneNode(true);
+		clon.id = "gameServer";
+		clon.children[0].children[0].innerHTML = games[i].name;
+		clon.children[0].children[1].innerHTML = games[i].playerCount + "/10 players";
+		clon.children[0].children[2].id = games[i].id;
+		clon.style.display = "inline-block";
+		$("#serverCardHolder")[0].insertBefore(clon, $("#serverExample")[0]);
+	}
+});
+
+function startGame()
+{
+	socket.emit('startGame', {});
+}
+
+showJoinableGames();
+setInterval(function()
+{
+	showJoinableGames();
+}, 5000);
--- server.js
+++ server.js
@@ -16,8 +16,19 @@ function createPlayer(player)
 	player.id = Math.random();
 	player.moveVector = [0, 0, 0, 0];
 	player.gameId = "";
+	player.game = undefined;
 	players[player.socketId] = player;
 	playerList.push(player);
+}
+
+function createGame(game)
+{
+	game.players = [];
+	game.enemies = [];
+	game.projectiles = [];
+	game.id = Math.random();
+	game.active = false;
+	games.push(game);
 }
 
 var userList = [];
@@ -46,6 +57,27 @@ io.on('connection', function(socket)
 	socket.on('disconnect', function()
 	{
 		///remove player from player list as well
+		for (var i = 0; i < games.length; i++)
+		{
+			var game = games[i];
+			var gamePlayers = game.players;
+			for (var a = 0; a < game.players.length; a++)
+			{
+				if (gamePlayers[a].socketId == socket.id)
+				{
+					gamePlayers.splice(a, 1);
+					//send notification to all players that player left game?//
+				}
+			}
+		}
+		for (var i = 0; i < playerList.length; i++)
+		{
+			if (playerList[i].socketId == socket.id)
+			{
+				playerList.splice(i, 1);
+				break;
+			}
+		}
 		delete players[socket.id];
 	});
 
@@ -67,5 +99,58 @@ io.on('connection', function(socket)
 			socket.emit('usernameResponse', {success: true});
 		}
 		else { socket.emit('usernameResponse', {success: false}); }
+	});
+	
+	socket.on('createServer', function(data)
+	{
+		var serverName = data.serverN || "";
+		var game = [];
+		game.name = serverName;
+		game.host = player;
+		createGame(game);
+		game.players.push(player);
+		player.game = game;
+		player.gameId = game.id;
+		
+		socket.emit('createServerResponse', {success: true});
+	});
+	
+	socket.on('joinGame', function(data)
+	{
+		var serverName = data.serverName;
+		console.log("Player: " + player.name + " is trying to join server: " + serverName);
+		var game = undefined;
+		for (var i = 0; i < games.length; i++)
+		{
+			if (games[i].name == serverName)
+			{
+				game = games[i];
+				break;
+			}
+		}
+		if (game == undefined)
+		{
+			socket.emit('joinGameResponse', {success: false, reason: "Game DNE"});
+			return;
+		}
+		game.players.push(player);
+		player.game = game;
+		player.gameId = game.id;
+		
+		socket.emit('joinGameResponse', {success: true});
+	});
+	
+	socket.on('getJoinableGames', function(data)
+	{
+		var gamesList = [];
+		for (var i = 0; i < games.length; i++)
+		{
+			if (games[i].active == false && games.players.length < 10) 
+			{
+				gamesList.push({name: games[i].name, playerCount: games[i].players.length, id: games[i].id}); 
+			}
+		}
+		//console.log(JSON.stringify(gamesList));
+		socket.emit('getJoinableGamesResponse', {success: true, games: gamesList});
 	});
 });
